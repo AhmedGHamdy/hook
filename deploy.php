944e0ccd572439cf749a4a8cf559c0ddf76726fe
AhmedGHamdy <60747498+AhmedGHamdy@users.noreply.github.com>
2020-12-07 14:30:14 +0200

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,1 +1,5 @@
-<?php `git pull`;
+<?php       
+    //echo "THis is a test file on a test repo for testing the deploy functionality using github webhooks!!!!";
+    echo exec('whoami');
+    echo exec('sh -x /opt/tomcat7_addtest/webapps/Add-Pluses/hook/shell.sh');
+?>
